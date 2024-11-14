4ed6ae0ffdce2916a4b7552c908b694abd90300f
Vladyslav1811 <vladkiriluk16@gmail.com>
2024-11-14 21:29:32 +0100

    Add files via upload

---
+++ home work 10 3.cs
@@ -0,0 +1,13 @@
+﻿class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a comma-separated string:");
+            string input = Console.ReadLine();
+            
+            string result = input.Replace(" ", "");
+            
+            Console.WriteLine("Result without spaces:");
+            Console.WriteLine(result);
+        } 
+    }
