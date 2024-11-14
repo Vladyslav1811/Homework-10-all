4193c46c01f1607cf2399b88f088ba1919bd378b
Vladyslav1811 <vladkiriluk16@gmail.com>
2024-11-14 21:42:48 +0100

    Add files via upload

---
+++ home work 10 2.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        StringBuilder report = new StringBuilder();
+
+        report.AppendLine("Event Report");
+        report.AppendLine("Date: " + DateTime.Now.ToShortDateString());
+        report.AppendLine("--------------------------");
+
+        Console.WriteLine("Enter events. Type 'done' to finish.");
+
+        while (true)
+        {
+            Console.Write("Enter event: ");
+            string eventInput = Console.ReadLine();
+
+            if (eventInput == "done")
+            {
+                break;
+            }
+
+            report.AppendLine("- " + eventInput);
+        }
+
+        Console.WriteLine("\nYour report:");
+        Console.WriteLine(report.ToString());
+    }
+}
