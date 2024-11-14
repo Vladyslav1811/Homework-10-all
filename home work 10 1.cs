2a776b43e81d4039346fc798ce7ef33f10a5ecb2
Vladyslav1811 <vladkiriluk16@gmail.com>
2024-11-14 21:35:11 +0100

    Add files via upload

---
+++ home work 10 1.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your first and last name:");
+        string[] nameParts = Console.ReadLine().Split(' ');
+
+        if (nameParts.Length < 2)
+        {
+            Console.WriteLine("Please enter both your first and last name.");
+            return;
+        }
+
+        if (nameParts[0][0] == nameParts[1][0])
+            Console.WriteLine("The last name starts with the same letter as the first name.");
+        else
+            Console.WriteLine("The last name does not start with the same letter as the first name.");
+    }
+}
